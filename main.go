571ef011be8cabb4d182a95515950e714eae306f
irynalukina <irinalukina90@gmail.com>
2022-01-12 12:32:46 +0200

    error

--- main.go
+++ main.go
@@ -19,6 +19,19 @@ func main() {
 		fmt.Println("Visiting", r.URL)
 	})
 
+	//c.OnHTML(`a[href]`, func(e *colly.HTMLElement) {
+	//	e.Request.Visit(e.Attr(".tabs__link"))
+	//})
+
+	// Find and visit next page links
+	c.OnHTML(`a:contains(' Характеристики ')`, func(e *colly.HTMLElement) {
+		e
+	})
+
+	c.OnRequest(func(e *colly.Request) {
+		fmt.Println("Charact", e.URL)
+	})
+
 	err := c.Visit("https://rozetka.com.ua/all-tv/c80037/sell_status=available;seller=rozetka/")
 	if err != nil {
 		return
