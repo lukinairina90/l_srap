b7aa436e824a6766efc820be110e05695456a3b3
A.Krasheninnikova <o.krasheninnikova@andersenlab.com>
2022-01-14 14:29:18 +0200

    Added characteristics to DB, added TODO

--- go.mod
+++ go.mod
@@ -3,19 +3,24 @@ module R_Scraper
 go 1.17
 
 require (
-
+	github.com/gocolly/colly v1.2.0
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.5
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/antchfx/htmlquery v1.2.4 // indirect
 	github.com/antchfx/xmlquery v1.3.9 // indirect
 	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20220111093109-d55c255bac03 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	ID   int
+	Name string
+}
+
+type ProductChar struct {
+	gorm.Model
+	ProductID int
+	Name      string
+	Value     string
+}
+
 func main() {
+	// смотрите https://github.com/go-sql-driver/mysql#dsn-data-source-name для подробностей
+	dsn := "root:@tcp(127.0.0.1:3306)/rozetka?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return
+	}
+
+	// Миграция схем
+	if err = db.AutoMigrate(&Product{}, &ProductChar{}); err != nil {
+		return
+	}
+
 	c := colly.NewCollector()
 	//Find and visit all links
 	c.OnHTML(".goods-tile__picture", func(e *colly.HTMLElement) {
@@ -20,20 +47,26 @@ func main() {
 	})
 
 	//Find and visit next page links
-	c.OnHTML(`.characteristics-full__list`, func(e *colly.HTMLElement) {
+	c.OnHTML(`.product-about__characteristics`, func(e *colly.HTMLElement) {
+		name := e.DOM.Find(".product-tabs__heading_color_gray").Text()
+		//todo write into products
+		product := Product{
+			Name: name,
+		}
+
+		db.Create(&product)
+
 		// Iterate over rows of the table which contains different information
 		// about the course
 		e.ForEach(".characteristics-full__item", func(_ int, el *colly.HTMLElement) {
-			label := el.DOM.Find(".characteristics-full__label span").Text()
-			char := el.DOM.Find("a.ng-star-inserted").Text()
+			charName := el.DOM.Find(".characteristics-full__label span").Text()
+			charValue := el.DOM.Find("a.ng-star-inserted").Text()
 
-			// todo insert value into db here
-			// add gorm https://gorm.io/docs/
-			// create model
-			// use model for insert
-			// brew install mysql
-			// remove this println
-			println(label + ": " + char)
+			db.Create(&ProductChar{
+				ProductID: product.ID,
+				Name:      charName,
+				Value:     charValue,
+			})
 		})
 	})
 
@@ -46,9 +79,15 @@ func main() {
 		}
 	})
 
-	err := c.Visit("https://rozetka.com.ua/all-tv/c80037/sell_status=available;seller=rozetka/")
+	err = c.Visit("https://rozetka.com.ua/all-tv/c80037/sell_status=available;seller=rozetka/")
 	if err != nil {
 		return
 	}
-	//todo add gorm orm
 }
+
+// todo add pictures folder(don`t forget about gitignore)
+// save all picures into this folder
+// add id from rozetka to products table (Код:  293091343) - original_id
+// after save of the new product if you will have any pictures,
+// create folder named as id from rozetka
+// put all pictures
